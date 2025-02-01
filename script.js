fb1728863f58941b7dbc1a238b8f56d7d209b666
meyerfreedman <m.freedman.student@centerschool.net>
2025-02-01 16:15:13 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+prompt("knock knock");
+let who = prompt("banana");
+for (let round = 1; round <= 4; round++){
+  who = prompt("banana. knock knock!");
+  who = prompt("banana");
+}
+who = prompt("orange");
+alert("orange you glad I didn't say banana?");
